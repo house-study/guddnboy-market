70a7cbe53e6ac641975dc0ff6273ea9713930229
guddnboy <hung3018@naver.com>
2025-04-09 08:30:06 +0900

    feat: 상품 목록 레이아웃 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
 const Home = () => {
+  {
+    /* [TODO] json-server에서 데이터 가져오는 것으로 수정 */
+  }
+  const TEMP_ARRAY = Array(16).fill(null);
+
   return (
     <div className="flex flex-col items-center justify-center">
       <header>
@@ -6,6 +11,23 @@ const Home = () => {
           <h1 className="text-center text-4xl font-bold">시장에 가면</h1>
         </div>
       </header>
+
+      <body>
+        <div className="grid grid-cols-4 gap-8">
+          {TEMP_ARRAY.map((_, index) => {
+            return (
+              <div
+                key={index}
+                className="flex transform flex-col rounded-lg p-4 shadow-sm transition-transform hover:scale-105 hover:cursor-pointer hover:shadow-2xl"
+              >
+                <div className="h-60 w-60 bg-gray-200"></div>
+                <div className="text-center text-lg">상품 이름</div>
+                <div className="text-center text-sm text-gray-500">가격</div>
+              </div>
+            );
+          })}
+        </div>
+      </body>
     </div>
   );
 };
