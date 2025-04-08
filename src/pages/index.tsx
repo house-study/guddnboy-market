5b51f932ce6e6f6d658837b9a4282f019b19e08d
guddnboy <hung3018@naver.com>
2025-04-09 08:29:27 +0900

    feat: 헤더 제목 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <header>
+        <div className="my-8">
+          <h1 className="text-center text-4xl font-bold">시장에 가면</h1>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Home;
