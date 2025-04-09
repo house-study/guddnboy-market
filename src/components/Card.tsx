c3c74f1e301a4bffd647b0ea42ca8ce69180eae3
guddnboy <hung3018@naver.com>
2025-04-09 22:46:23 +0900

    style: 불필요한 div 태그 삭제

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,21 +4,19 @@ const TEMP_ARRAY = Array(16).fill(null);
 const Card = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <div>
-        <div className="grid grid-cols-4 gap-8 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1">
-          {TEMP_ARRAY.map((_, index) => {
-            return (
-              <div
-                key={index}
-                className="flex transform flex-col rounded-lg p-4 shadow-sm transition-transform hover:scale-105 hover:cursor-pointer hover:shadow-2xl"
-              >
-                <div className="h-60 w-60 bg-gray-200"></div>
-                <div className="text-center text-lg">상품 이름</div>
-                <div className="text-center text-sm text-gray-500">가격</div>
-              </div>
-            );
-          })}
-        </div>
+      <div className="grid grid-cols-4 gap-8 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1">
+        {TEMP_ARRAY.map((_, index) => {
+          return (
+            <div
+              key={index}
+              className="flex transform flex-col rounded-lg p-4 shadow-sm transition-transform hover:scale-105 hover:cursor-pointer hover:shadow-2xl"
+            >
+              <div className="h-60 w-60 bg-gray-200"></div>
+              <div className="text-center text-lg">상품 이름</div>
+              <div className="text-center text-sm text-gray-500">가격</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
