50709de440340480dd3527b506786053a26d71c0
guddnboy <hung3018@naver.com>
2025-04-09 21:48:02 +0900

    refactor: header 컴포넌트 분리

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="my-8">
+        <h1 className="text-center text-4xl font-bold">시장에 가면</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
 import Card from '@/components/Card';
+import Header from '@/components/Header';
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <header>
-        <div className="my-8">
-          <h1 className="text-center text-4xl font-bold">시장에 가면</h1>
-        </div>
-      </header>
-
+      <Header />
       <Card />
     </div>
   );
