201cba90cc4851eb44b5829b6563e439c7c6162c
guddnboy <hung3018@naver.com>
2025-04-14 19:45:45 +0900

    refactor: 상품 데이터 json-server에서 가져오도록 수정

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,22 +1,49 @@
-/* [TODO] json-server에서 데이터 가져오는 것으로 수정 */
-const TEMP_ARRAY = Array(16).fill(null);
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const API_URL = 'http://localhost:3001';
 
 const ProductList = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/products`);
+      const data = response.data;
+      setProducts(data);
+      return;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="h-screen w-full max-w-7xl overflow-y-scroll p-4">
       <div className="grid grid-cols-4 flex-col items-center justify-center gap-8 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1">
-        {TEMP_ARRAY.map((_, index) => {
-          return (
-            <div
-              key={index}
-              className="flex transform flex-col rounded-lg p-4 shadow-sm transition-transform hover:scale-105 hover:cursor-pointer hover:shadow-2xl"
-            >
-              <div className="h-60 w-60 bg-gray-200"></div>
-              <div className="text-center text-lg">상품 이름</div>
-              <div className="text-center text-sm text-gray-500">가격</div>
+        {products?.map(product => (
+          <div
+            key={`${product.name}-${product.id}`}
+            className="flex transform flex-col rounded-lg p-4 shadow-sm transition-transform hover:scale-105 hover:cursor-pointer hover:shadow-2xl"
+          >
+            <img
+              className="h-60 w-60 bg-gray-200"
+              src={product.imageURL}
+              alt={product.name}
+            />
+            <div className="text-center text-lg">{product.name}</div>
+            <div className="text-center text-sm text-gray-500">
+              {product.price.toLocaleString('ko-KR', {
+                style: 'currency',
+                currency: 'KRW',
+              })}
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
