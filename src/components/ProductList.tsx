11b7d95d6ade8ceba412998a3490a8e7ae3a01a5
guddnboy <hung3018@naver.com>
2025-04-15 21:29:00 +0900

    refactor: 환경 변수 분리

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-const API_URL = 'http://localhost:3001';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const ProductList = () => {
   const [products, setProducts] = useState<Product[]>([]);
