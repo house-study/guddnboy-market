5ceda628735fbf5ca104c694f33df48c3b125591
guddnboy <hung3018@naver.com>
2025-04-17 10:17:57 +0900

    refactor: 에러 발생 시 에러 컴포넌트 표시

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,17 +1,28 @@
 import { useEffect, useState } from 'react';
 
 import { getProducts } from '@/api/products';
+import ErrorComponent from '@/components/ErrorComponent';
 
 const Products = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [isError, setIsError] = useState<Error | null>(null);
 
   useEffect(() => {
     const loadProducts = async () => {
-      const productsData = await getProducts();
-      setProducts(productsData);
+      try {
+        const productsData = await getProducts();
+        setProducts(productsData);
+      } catch (error) {
+        setIsError(error as Error);
+      }
     };
+
     loadProducts();
   }, []);
+
+  if (isError) {
+    return <ErrorComponent message={isError.message} />;
+  }
 
   return (
     <div className="h-screen w-full max-w-7xl overflow-y-scroll p-4">
