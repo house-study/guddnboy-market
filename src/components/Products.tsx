f23910c3cf7d32fbd205db4defa05b3fb26a43aa
guddnboy <hung3018@naver.com>
2025-04-21 22:42:05 +0900

    style: 클릭 포인터 스타일 수정

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -31,7 +31,7 @@ const Products = () => {
       <div className="grid grid-cols-4 flex-col items-center justify-center gap-8 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1">
         {products?.map(product => (
           <Link href={`/product/${product.id}`} key={product.id}>
-            <div className="flex transform flex-col rounded-lg p-4 shadow-sm transition-transform hover:scale-105 hover:cursor-pointer hover:shadow-2xl">
+            <div className="flex transform cursor-pointer flex-col rounded-lg p-4 shadow-sm transition-transform hover:scale-105 hover:shadow-2xl">
               <img
                 className="h-60 w-60 bg-gray-200"
                 src={product.imageURL}
