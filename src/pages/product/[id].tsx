38744d0f91114bdfed2dea0cc5972412e3d4da27
guddnboy <hung3018@naver.com>
2025-04-25 11:45:38 +0900

    refactor: 에러 처리 로직 수정 및 메세지 분리

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -15,11 +15,20 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
         product,
       },
     };
-  } catch (error: unknown) {
+  } catch (error: Error | unknown) {
+    if (error instanceof Error) {
+      return {
+        props: {
+          error: {
+            message: error.message,
+          },
+        },
+      };
+    }
     return {
       props: {
         error: {
-          message: (error as Error).message,
+          message: '알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요.',
         },
       },
     };
