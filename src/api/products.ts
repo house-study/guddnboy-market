29f457d328ad2fe104bbee9ef034d3974ad66e04
guddnboy <hung3018@naver.com>
2025-04-27 22:31:10 +0900

    feat: 장바구니 관련 API 함수 제거

--- src/api/products.ts
+++ src/api/products.ts
@@ -21,26 +21,3 @@ export const getProductDetail = async (id: number) => {
     throw new Error('상품 상세 정보를 불러오는 데 실패했습니다.');
   }
 };
-
-export const getCartProducts = async () => {
-  try {
-    const response = await axios.get<CartItem[]>(`${API_URL}/cart`);
-    const data = response.data;
-    return data;
-  } catch (error) {
-    throw new Error('장바구니 정보를 불러오는 데 실패했습니다.');
-  }
-};
-
-export const addToCart = async (product: Product, quantity: number) => {
-  try {
-    const response = await axios.post(`${API_URL}/cart`, {
-      ...product,
-      quantity,
-    });
-    const data = response.data;
-    return data;
-  } catch (error) {
-    throw new Error('장바구니에 상품을 추가하는 데 실패했습니다.');
-  }
-};
