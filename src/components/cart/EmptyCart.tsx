a7f1af575a2844a82ef96d68875d67e791c9307c
guddnboy <hung3018@naver.com>
2025-04-30 11:43:05 +0900

    style: 불필요한 여백 제거

--- src/components/cart/EmptyCart.tsx
+++ src/components/cart/EmptyCart.tsx
@@ -7,7 +7,6 @@ export const EmptyCart = () => {
   return (
     <div className="mx-auto max-w-screen-lg items-center py-8 text-center text-gray-400">
       <p>원하는 상품을 장바구니에 담아보세요.</p>
-
       <button
         onClick={handleGoToHome}
         className="mt-6 rounded bg-gray-700 px-6 py-2 text-white transition-colors hover:bg-gray-800"
