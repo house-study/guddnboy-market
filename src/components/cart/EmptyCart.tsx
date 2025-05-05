79de99d31cc871bc9ca5018242e9cef4b3ae5986
guddnboy <hung3018@naver.com>
2025-05-05 16:37:28 +0900

    feat: 장바구니 비어있을 때 표시할 컴포넌트 추가

---
+++ src/components/cart/EmptyCart.tsx
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/router';
+
+export const EmptyCart = () => {
+  const router = useRouter();
+  const handleGoToHome = () => {
+    router.push('/');
+  };
+
+  return (
+    <div className="mx-auto max-w-screen-lg items-center py-8 text-center text-gray-400">
+      <h2 className="mb-4 text-xl font-semibold">장바구니가 비어있습니다</h2>
+      <p className="mb-8">원하는 상품을 장바구니에 담아보세요.</p>
+      <button
+        onClick={handleGoToHome}
+        className="mt-6 cursor-pointer rounded bg-gray-700 px-6 py-2 text-white transition-colors hover:bg-gray-800"
+      >
+        상품 보러가기
+      </button>
+    </div>
+  );
+};
