15ae1eaf1ee7c5dde5ab7bd9f6a57cf1626da03d
guddnboy <hung3018@naver.com>
2025-05-07 15:04:07 +0900

    feat: 장바구니 상품 데이터 타입 추가

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -7,3 +7,13 @@ interface Product {
   price: number;
   amount: number;
 }
+
+interface CartItem {
+  id: string;
+  imageURL: string;
+  name: string;
+  origin: string;
+  price: number;
+  amount: number;
+  quantity: number;
+}
